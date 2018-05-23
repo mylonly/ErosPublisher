2face057f2307c90f7d071d41a22fea503a39710
Mylonly <root@mylonly.com>
2018-05-23 16:59:43 +0800

    修改upload

--- Package/views.py
+++ Package/views.py
@@ -14,6 +14,8 @@ import json
 from rest_framework.permissions import AllowAny
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authentication import SessionAuthentication,BasicAuthentication
+
 # Create your views here.
 
 
